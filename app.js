ab61264e2c681980d24020d26266699bc76500a1
Richard Lefevre <rlefevre.6505@gmail.com>
2025-11-27 22:51:19 +0000

    feat: added funtion with loop to render img thumbnails to relevant div

--- app.js
+++ app.js
@@ -3,43 +3,53 @@
 //1. store image data:
 const imageData = [
   {
-    imageName: "#",
-    imageSRC: "#",
-    imageAlt: "#",
+    imageName: "dolphins",
+    imageSRC: "images/dolphins.jpg",
+    imageAlt: "Dolphins swimming",
   },
   {
-    imageName: "#",
-    imageSRC: "#",
-    imageAlt: "#",
+    imageName: "humpback",
+    imageSRC: "images/humpback.jpg",
+    imageAlt: "A whale breaching",
   },
   {
-    imageName: "#",
-    imageSRC: "#",
-    imageAlt: "#",
+    imageName: "heron",
+    imageSRC: "images/heron.jpg",
+    imageAlt: "A heron flying over water",
   },
 ];
 
+let thumbnailContainer = document.getElementById("thumbnail-container");
+
 //2. create thumbnail images:
+//select DOM element (thumbnail container) to contain thumbnails
+//this is repetative, so a for loop (for of/each?) can be used
+//the loop task is to create an img elemt and to update the src and alt propertiers, to match those in the array (parameters)
+//give each img a class name
+
 function createThumbnails() {
-  //select DOM element (thumb container) to contain thumbnails
-  //this is repetative, so a for loop (for of/each?) can be used
-  //the loop task is to create an img elemt and to update the src and alt propertiers, to match those in the array (parameters)
-  //give each img a class name
-  //add event listener to each image
-  //add event listener to each img. the event handler is the function I write to create the fullscreen images
-  //append created images to the thumbnail container
+  for (let i = 0; i < imageData.length; i++) {
+    const thumb = document.createElement("img");
+    thumb.src = imageData[i].imageSRC;
+    thumb.classList.add("thumbnail");
+    thumbnailContainer.appendChild(thumb);
+  }
 }
 
+//add event listener to each img. the event handler is the function I write to create the fullscreen images
+thumb.addEventListener("click", createFullscreenImage());
+
 //3. create fullscreen images:
-function createFullscreenImage() {
-  //this is the event handler
-  //select the fullscreen container
-  //delete current fullscreen image
-  //fullscreenConatiner.innerHTML="" or null
-  //create image, update values (properties)
-  //add classname so it can be styled to be fullscreen
-  //appent to container
-}
+//this is the event handler
+//select the fullscreen container
+//delete current fullscreen image
+//fullscreenConatiner.innerHTML="" or null
+//create image, update values (properties)
+//add classname so it can be styled to be fullscreen
+//appent to container
+function createFullscreenImage() {}
 
 //add the createFullscreenImage function as the event handler of the above event
 //call createThumbnails function
+
+createThumbnails();
